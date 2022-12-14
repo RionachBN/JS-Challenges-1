317320e27344d29651bcd172fa4571dbf56c4b66
RionachBN <rionachnnochiri@hotmail.com>
2022-12-14 16:35:02 +0000

    firstcommit

---
+++ arrays.js
@@ -0,0 +1,18 @@
+let favFilms = [
+"Silver Linings Playbook" ,
+"Goodfellas" ,
+"Get out"
+]
+console.log(favFilms)
+console.log(favFilms[2])
+favFilms[1] = "The Menu"
+console.log(favFilms)
+console.log(favFilms.length)
+console.log(favFilms.pop())
+console.log(favFilms)
+console.log(favFilms.shift())
+console.log(favFilms)
+console.log(favFilms.slice())
+console.log(favFilms)
+console.log(favFilms.unshift())
+console.log(favFilms)
