d41f6e0da47f7078f8030e567c5be7c71288a663
RionachBN <rionachnnochiri@hotmail.com>
2022-12-15 09:48:47 +0000

    variabled

---
+++ variables.js
@@ -0,0 +1,27 @@
+console.log ("Hello world")
+console.log ("firstName")
+let firstName = "John"
+let age = 26
+let universityStudent = true
+console.log (firstName)
+console.log (age)
+console.log (universityStudent)
+firstName = "Lucy"
+console.log (firstName)
+const lastName = "Matthews" 
+console.log (lastName)
+let modelOfcar = "Hyundai"  
+console.log ("I drive a " + modelOfcar)
+console.log ("Hi my name is" + firstName + "I drive a " + modelOfcar)
+console.log (`Hi my name is ${firstName}. I drive a ${modelOfcar}.`)
+let breakfastfood = "toast"
+let lunchfood = "sandwich"
+let dinnerfood = "pasta"
+console.log (`I will have  ${breakfastfood} a ${lunchfood} and ${dinnerfood} `)
+let a = 5
+console.log (a += 3)
+const myInt = 5
+console.log (typeof myInt)
+const num1 = 4
+const num2 = 6
+console.log (num1 * 5) 
