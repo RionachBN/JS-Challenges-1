dfe7679abf81b1f0e3515551107223de0e9fb536
RionachBN <rionachnnochiri@hotmail.com>
2022-12-15 09:44:55 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,75 @@
+// let faveHolidayDestinations = [
+//     "Chile",
+//     "New Zealand" , 
+//     "Rome"
+// ]
+// console.log(faveHolidayDestinations)
+// for (let i = 1; i < faveHolidayDestinations.length; i++) {
+//     console.log(faveHolidayDestinations [i])
+// }
+
+// for (let i=0; i <= 10; i++) {
+// console.log(i += 2)
+// }
+// // let evenNumbers = []
+// // for (let n = 0; n < 20; n++) {
+// //     if (n % 2 ==0) {
+// //         (evenNumbers.push(n))
+// //     }
+// // }
+// // console.log(`The even numbers between 0 and 20 are; ${evenNumbers}`)
+
+// // let revEvenNumbers = []
+// // for (let n = 20; n > 0; n--) {
+// //     if (n % 2 ==0) {
+// //         (revEvenNumbers.push(n))
+// //     }
+// // }
+// // console.log (`The even numbers between 20 and 0 are ${revEvenNumbers}`)
+
+
+
+// // // let lives = 3
+// // // while (lives > 0) {
+// // //     console.log("Well done! You're still in the game.")
+// // //     lives--
+// // // }
+// // // console.log("Game Over!!")
+// // // let currentdiceroll = 3
+// // // while (currentdiceroll != 1) {
+// // //     console.log (currentdiceroll)
+// // //     currentdiceroll = Math.floor(Math.random()*6)+1
+// // // }
+// // // console.log(currentdiceroll)
+// // Actvity 3
+// let oddNumbers = []
+// for (let n = 0; n < 30; n++) {
+//     if (n % 2 != 0) {
+//         (oddNumbers.push (n))
+//     }
+// }
+// console.log(`The odd numbers between 0 and 30 are ${oddNumbers}`)
+// // Activity 4
+// let age = Math.floor(Math.random()*100)
+// while(age <= 18){
+//     console.log("You're a child!)")
+//     age ++
+//     }
+
+// console.log("You're an adult!")
+// // Activity 4
+// let numbers = []
+// while(numbers.length < 6) {
+//     randomNumber = Math.floor(Math.random()*1000)
+// console.log(randomNumber)
+// numbers.push(randomNumber)
+// }
+// Acivity 5
+let films = ["Hot Fuzz", "Dear John", "Baby Driver", "Black Panther"]
+// for(filmNumber==0; filmNumber < films.length; filmNumber++)
+    if(films[2]=="Ghostbusters"){
+        console.log("Yay it's Ghostbusters!")
+    }
+else if(films[2]!= "Ghostbusters"){
+    console.log("Boo. We want Ghostbusters!")
+}
