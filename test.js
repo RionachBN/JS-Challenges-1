d91535f1fdcd6a78c706a212f6ba8174233a524d
RionachBN <rionachnnochiri@hotmail.com>
2022-12-15 09:47:47 +0000

    test

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log ("Hello".length)
+console.log ("Hello, what is your name?".length)
+console.log ("Hello".toUpperCase())
+// this is known as a statement that is not affecting the rest
+console.log ("My name is Rionach".toLowerCase())
+console.log ("I like dogs".trim())
+console.log ("77".toString())
+console.log ("Hello my name is Rionach".includes("Hello"))
